1553f733568f2de4ef73025605963a314bc19852
Mehrdadmg <m.majidzadeh.g@gmail.com>
2024-03-25 14:19:42 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    // <img src={logo} className="App-logo" alt="logo" />
+    <div className="App"></div>
+  );
+}
+
+export default App;
